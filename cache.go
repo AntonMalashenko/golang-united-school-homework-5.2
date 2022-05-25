ccef484b42824080d1eecff5685bbebff1f5cfc5
anton_malashenko <anton.mal.slb@gmail.com>
2022-05-25 22:51:47 +0300

    implement put get keys

--- cache.go
+++ cache.go
@@ -3,21 +3,32 @@ package cache
 import "time"
 
 type Cache struct {
+	data map[string][]string
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{make(map[string][]string)}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	value, ok := c.data[key]
+	if ok {
+		return value[0], true
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.data[key] = []string{value, ""}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	keys := make([]string, 0)
+	for k, _ := range c.data {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
 }
