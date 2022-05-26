abd49ec3d9a2bf45ce6c45b7b130110182f28af4
anton_malashenko <anton.mal.slb@gmail.com>
2022-05-26 16:58:56 +0300

    implement PutTill

--- cache.go
+++ cache.go
@@ -2,24 +2,30 @@ package cache
 
 import "time"
 
+type valueItem struct {
+	value string
+	date  time.Time
+}
+
 type Cache struct {
-	data map[string][]string
+	data map[string]valueItem
 }
 
 func NewCache() Cache {
-	return Cache{make(map[string][]string)}
+	return Cache{make(map[string]valueItem)}
 }
 
 func (c Cache) Get(key string) (string, bool) {
-	value, ok := c.data[key]
+	valueItem, ok := c.data[key]
 	if ok {
-		return value[0], true
+		return valueItem.value, true
 	}
+	c.cleanExpired()
 	return "", false
 }
 
 func (c Cache) Put(key, value string) {
-	c.data[key] = []string{value, ""}
+	c.PutTill(key, value, time.Time{})
 }
 
 func (c Cache) Keys() []string {
@@ -27,9 +33,20 @@ func (c Cache) Keys() []string {
 	for k, _ := range c.data {
 		keys = append(keys, k)
 	}
+	c.cleanExpired()
 	return keys
 }
 
 func (c Cache) PutTill(key, value string, deadline time.Time) {
-	//	function empty
+	c.data[key] = valueItem{value: value, date: deadline}
+	c.cleanExpired()
+}
+
+func (c Cache) cleanExpired() {
+	now := time.Now()
+	for key, valueItem := range c.data {
+		if !valueItem.date.IsZero() && valueItem.date.Before(now) {
+			delete(c.data, key)
+		}
+	}
 }
